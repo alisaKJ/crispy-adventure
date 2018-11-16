79b41fced522870223e61715d6d7a6033cab18c1
Alisa Jones <lostpye@aol.com>
2018-11-16 03:02:25 -0500

    add color to songButton, footer

---
+++ site.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var txt;
+    var person = prompt("Please type your favorite song:", "Sweet potato");
+    if (person == null || person == "") {
+        txt = "Oh.  Well, Just sing in the car to your kids.";
+    } else {
+        txt = "I like to sing " + person + " too!!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
